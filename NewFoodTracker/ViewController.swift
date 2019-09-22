4d0a0b55719e6d74eac19650970fc1595aacc599
Farouk <farouk.talatini@gmail.com>
2019-09-22 16:53:25 -0400

    Initial Commit

---
+++ NewFoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewFoodTracker
+//
+//  Created by Farouk Talatini on 9/22/19.
+//  Copyright © 2019 Farouk Talatini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
